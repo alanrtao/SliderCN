f0b59e727c411465bf2f7bff4e5233baf9cdc62c
alanrtao <alanrtao@outlook.com>
2025-05-13 18:21:49 -0400

    screening

--- sanity.py
+++ sanity.py
@@ -1,16 +1,14 @@
 from os import path, listdir
 from textwrap import indent
 from fontTools.ttLib import TTFont
-from fontTools.unicode import Unicode
 
 import csv
 
 from dataclasses import dataclass
 
-font_latin = './fusion-pixel-12px-proportional-latin.ttf'
-font_zh = './fusion-pixel-12px-proportional-zh_hans.ttf'
+font_latin = 'fusion-pixel-12px-proportional-latin.ttf'
+font_zh = 'fusion-pixel-12px-proportional-zh_hans.ttf'
 
-fonts = (TTFont(font_latin), TTFont(font_zh))
 parent = path.realpath(path.dirname(__file__))
 
 header_row_count = 4 # format comments, config name, config val, column names
@@ -26,8 +24,26 @@ class Record:
     translation: str
     metadata: str
 
-def char_not_in_fonts(c: str):
-    u = ord(c)
+@dataclass
+class FontDescriptor:
+    path: str
+    usage: set[str]
+
+fonts: dict[TTFont, FontDescriptor] = { TTFont(p): FontDescriptor(p, set()) for p in (font_latin, font_zh) }
+
+def char_in_font(c: str, f: TTFont) -> bool:
+    cmaps = f['cmap'].tables
+    for cm in cmaps:
+        # if cm.isUnicode():
+        if ord(c) in cm.cmap:
+            fonts[f].usage.add(c)
+            return True
+    return False
+
+def char_not_in_fonts(c: str) -> bool:
+    for f in fonts.keys():
+        if char_in_font(c, f): return False
+    return True
 
 with open(err_log, mode='w') as log_file:
 
@@ -75,3 +91,7 @@ with open(err_log, mode='w') as log_file:
                         log_curr_error(f'Character not supported by font `{c}`')
                 
         
+for font, desc in fonts.items():
+    with open(f'{desc.path}.log', mode='w') as w:
+        for u in sorted(desc.usage):
+            print(u, end='', file=w)
